cb237ac3bd2f07284d31c57303b3363bd43463d9
Thomas Leela <thomas.leela@epinova.no>
2020-03-05 08:24:32 +0100

    Add code comments.

--- FizzBuzzJazzFuzz/ServicesFactory/Services/JazzFuzz.cs
+++ FizzBuzzJazzFuzz/ServicesFactory/Services/JazzFuzz.cs
@@ -1,6 +1,10 @@
 ﻿using FizzBuzzJazzFuzz.InputOutput;
 
 namespace FizzBuzzJazzFuzz.ServicesFactory.Services {
+
+    //Denne filen er veldig lik FizzBuzz.cs Vi har varslet at det vil komme flere spill i fremtiden.
+    //Da er det ikke ønskelig å trykke opp flere nesten like kopier av disse filene.
+    //Klarer du å konsolidere slik at du skiller regler fra selv spillmotor?
     public class JazzFuzz : IBusinessService {
         private const int MAXLENGTH = 100, MINLENGTH = 1;
         private IOConsole io;
